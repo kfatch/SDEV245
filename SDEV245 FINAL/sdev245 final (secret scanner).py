347d8f32b16924993a067b0ef9b42481e84d49af
kfatch <kfatch@ivytech.edu>
2026-05-09 10:15:15 -0400

    Added Changes

--- SDEV245 FINAL/sdev245 final (secret scanner).py
+++ SDEV245 FINAL/sdev245 final (secret scanner).py
@@ -6,39 +6,41 @@
 import re
 import os
 import argparse
+from pathlib import Path
 
 #---------Functions-----------------
+def file_scan(file_path, patterns):
+    secrets = []
+    compiled_patterns = [re.compile(p) for p in patterns]
+    try:
+        with open(file_path, "r") as f:
+            for line_num, line in enumerate(f, 1):
+                for pat in compiled_patterns:
+                    if pat.search(line):
+                        secrets.append({
+                            "line": line_num,
+                            "content": line.strip(),
+                            "pattern": pat.compiled_patterns
+                        })
+    except FileNotFoundError:
+        print(f"{file_path} not found.")
 
-def file_scan(file_path):
-    patterns = [(r"(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:.(?!.))){0,28}(?:[A-Za-z0-9_]))?)"),
-                (r"(?:#)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:.(?!.))){0,28}(?:[A-Za-z0-9_]))?)"),
-                (r"AIza[0-9A-Za-z-_]{35}"),
-                (r"^ghp_[a-zA-Z0-9]{36}$"),
-                (r"^ghr_[a-zA-Z0-9]{36}$")
-                ]
-    secrets = {}
-    with open(file_path, "r", encoding="utf-8") as f:
-        for line_num, line in enumerate(f, 1):
-            # for  in patterns.items():
-            #     if re.search(pattern, line):
-            #         secrets.append[])
     return secrets
+
+#-------------Lists-------------------
+pattern_list = {"Instagram Username": r"(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:.(?!.))){0,28}(?:[A-Za-z0-9_]))?)",
+                "Instagram Hashtag": r"(?:#)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:.(?!.))){0,28}(?:[A-Za-z0-9_]))?)",
+                "Google API": r"AIza[0-9A-Za-z-_]{35}",
+                "Github PAT": r"^ghp_[a-zA-Z0-9]{36}$",
+                "Github Refresh Token": r"^ghr_[a-zA-Z0-9]{36}$"}
 
 #----------Main-Program-------------
 print("Starting Secret Scanner...")
-# file_path = input("Enter the file path to be scanned:\n") Commented out for testing
-file_path = "C:/Users/knfat/OneDrive/Desktop/SchoolResources/sdev245_final_secrets.txt"
-if os.path.exists(file_path):
-    results = file_scan(file_path)
-    if results:
-        print(results)
-    else:
-        print("No secrets found in given file path.")
+file_path = r"C:\Users\knfat\OneDrive\Desktop\SchoolResources\Security_and_Secure_Coding\SDEV245 FINAL\sdev245_final_secrets.txt"
+results = file_scan(file_path, pattern_list)
+if results == []:
+    print("No secrets found.")
+else:
+    for match in results:
+        print(match)
 print("Ending Program...")
-
-#-----------Test--------------
-# patterns = {"Instagram Username": r"(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:.(?!.))){0,28}(?:[A-Za-z0-9_]))?)",
-#                 "Instagram Hashtag": r"(?:#)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:.(?!.))){0,28}(?:[A-Za-z0-9_]))?)",
-#                 "Google API": r"AIza[0-9A-Za-z-_]{35}",
-#                 "Github PAT": r"^ghp_[a-zA-Z0-9]{36}$",
-#                 "Github Refresh Token": r"^ghr_[a-zA-Z0-9]{36}$"}
